4a604e9d187e5c6622e651b7911490fc64f63f0f
Coder123344 <68776148+Coder123344@users.noreply.github.com>
2020-08-18 19:09:06 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# KABOOM112345678
+# bouncyBall
+Bouncy Ball created using matter.js physics engine
---
+++ sketch.js
@@ -0,0 +1,43 @@
+var ground, ball;
+
+
+function setup(){
+    var canvas = createCanvas(400,400);
+    
+
+    var ground_options ={
+        isStatic: true
+    }
+
+    ground = Bodies.rectangle(200,390,200,20,ground_options);
+    World.add(world,ground);
+    
+    var options ={
+        istatic: false,
+        restitution: 0.3,
+        friction: 0.5,
+        density: 1.2
+    }
+    ball = Bodies.circle(100,100,20,options);
+    World.add(world,ball);
+}
+
+function draw(){
+    background(0);
+    rectMode(CENTER);
+    fill("brown");
+    rect(ground.position.x,ground.position.y,400,20);
+
+    ellipseMode(RADIUS);
+    fill("blue");
+    ellipse(ball.position.x, ball.position.y, 20, 20);
+}
+    if (UP_ARROW.IsPressed) {
+        keyPressed();
+    }
+
+function keyPressed() {
+    if (keyCode === UP_ARROW) {
+        Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:85})
+    }
+}
